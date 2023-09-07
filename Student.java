4c337e4523d2f78aeafcb1a9845d7a9e032cdd8f
kausk31 <kausk259@gmail.com>
2023-09-07 20:09:33 +0530

    student file is added

---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student{
+
+    public Student(){
+        System.out.println("creatung studnt object");
+    }
+
+    public void sayhello(){
+        System.out.println("hi i am new stiudnt");
+    }
+}
