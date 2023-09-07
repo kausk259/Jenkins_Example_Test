5b527f6f6bf4484583253a537f61fb49053d2e5a
kausk31 <kausk259@gmail.com>
2023-09-07 20:13:47 +0530

    changes in main

--- Main.java
+++ Main.java
@@ -6,5 +6,9 @@ public class Main {
     System.out.println("date of current system is ");
     System.out.println(new Date());
     System.out.println("added one more file");
-    }
+    Student s = new Student();
+    
+    s.sayhello();
+
+}
 }
